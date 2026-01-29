15bae36c00c73d8292e6b8c1346a46098e40ccb2
NGUYENSONTAY <nguyensontay365@gmail.com>
2026-01-29 07:36:11 +0700

    Add C program of <110122155_NguyenSonTay>

---
+++ 110122155.cpp
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    // Nhap hai so nguyen
+    printf("Nhap so thu nhat: ");
+    scanf("%d", &a);
+    printf("Nhap so thu hai: ");
+    scanf("%d", &b);
+
+    // In ket qua
+    printf("Tong: %d\n", a + b);
+    printf("Hieu: %d\n", a - b);
+    printf("Tich: %d\n", a * b);
+
+    if (b != 0) {
+        printf("Thuong: %.2f\n", (float)a / b);
+    } else {
+        printf("Khong the chia cho 0\n");
+    }
+
+    return 0;
+}
+
